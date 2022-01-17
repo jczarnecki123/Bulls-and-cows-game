7bcb2dc1bfaeb0156a64139a774f73c188fe6c98
jczarnecki123 <jczarnecki4k2@gmail.com>
2022-01-17 14:47:47 +0100

    changed

--- BullsAndCows.py
+++ BullsAndCows.py
@@ -2,31 +2,37 @@ import random
 # from winreg import CloseKey
 
 def generate_Number():
-    global generated_Number
-    generated_Number = []
-    for x in range(0, 4):
-        generated_Number.append(random.randint(0,9))
-    return generated_Number
+    global generated_Number_List
+    generated_Number_List = []
+    while len(generated_Number_List) != 4:
+        generated_Number = random.randint(0,9)
+        if generated_Number not in generated_Number_List:
+            generated_Number_List.append(generated_Number)
+    return generated_Number_List
 
 def compare_Numbers():
     tries = 0
-    while(True):
-        tries += 1
+    while True:
         bulls = 0
         cows = 0
-        user_Number = input("Type your number:\n>>")
-        user_Number_Check = list(map(int, user_Number))
-        check_Place = 0
-        for x in user_Number_Check:
-            if x in generated_Number and user_Number_Check[check_Place] != generated_Number[check_Place]:
-                bulls += 1
-            elif x in generated_Number and user_Number_Check[check_Place] == generated_Number[check_Place]:
-                cows += 1
-            check_Place += 1
-        print("You have",cows,"cows and",bulls,"bulls")
-        if cows == 4:
-            print("Congratulations! You win \nIt took you",tries,"tries to get the right number")
-            break
+        user_Number = input("Type your 4-digit number:\n>>")
+        if len(user_Number) != 4:
+            print("Error! You have to enter 4 - digit number")
+        else:
+            tries += 1
+            user_Number_Check = list(map(int, user_Number))
+            check_Place = 0
+            for x in user_Number_Check:
+                if x in generated_Number_List and user_Number_Check[check_Place] != generated_Number_List[check_Place]:
+                    bulls += 1
+                elif x in generated_Number_List and user_Number_Check[check_Place] == generated_Number_List[check_Place]:
+                    cows += 1
+                check_Place += 1
+            print("You have {} cows and {} bulls".format(cows, bulls))
+            print()
+            if cows == 4:
+                print("Congratulations! You win \nIt took you {} tries to get the right number".format(tries))
+                break
 
 def play_Again():
     ask_User = input("Do you want to play again?\n[Yes]/[No]")
